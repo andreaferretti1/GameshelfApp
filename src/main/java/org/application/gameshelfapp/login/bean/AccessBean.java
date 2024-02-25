547a201d95490ea915f3eceed8a8dd8c056f1e65
andreaferretti1 <fer.andrea35@gmail.com>
2024-02-25 16:04:59 +0100

    "revisione code smells"

--- src/main/java/org/application/gameshelfapp/login/bean/AccessBean.java
+++ src/main/java/org/application/gameshelfapp/login/bean/AccessBean.java
@@ -11,14 +11,12 @@ public class AccessBean {
     private final String passwordBean;
 
     protected String emailRegex =  "[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}$";
-    protected String pwdRegex = "[a-zA-Z0-9._%&?^!]+";
     protected String usernameRegex = "[a-zA-Z0-9._]+";
 
     public AccessBean(String emailBean, String passwordBean) throws SyntaxErrorException {
 
         this.checkSyntax(emailBean, emailRegex, "email");
         this.emailBean = emailBean;
-        this.checkSyntax(passwordBean, pwdRegex, "password");
         this.passwordBean = passwordBean;
     }
 
@@ -36,9 +34,7 @@ public class AccessBean {
         boolean b = m.matches();
         if(!b && subject.equals("email")) {
             throw new SyntaxErrorException("Invalid syntax of email address");
-        } else if(!b && subject.equals("password")){
-            throw new SyntaxErrorException("Invalid syntax of password. You can use letters, numbers, and .,_,%,&,?,^,!");
-        } else if(!b && subject.equals("username")){
+        }  else if(!b && subject.equals("username")){
             throw new SyntaxErrorException("Invalid syntax of username. You can use numbers, letters, and . or _");
         }
     }
