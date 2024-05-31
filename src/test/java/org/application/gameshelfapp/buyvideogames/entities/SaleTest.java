f1c53e4bd702bedf000d05dbf732c342299474f4
andreaferretti1 <fer.andrea35@gmail.com>
2024-06-01 00:26:47 +0200

    "Modificato codice e aggiunto test"

--- src/test/java/org/application/gameshelfapp/buyvideogames/entities/SaleTest.java
+++ src/test/java/org/application/gameshelfapp/buyvideogames/entities/SaleTest.java
@@ -24,6 +24,53 @@ class SaleTest {
         sale.setVideogameSold(videogame);
         assertEquals(videogame, sale.getVideogameSold());
     }
-    @Test
 
+    @Test
+    void getAddress() {
+        Sale sale = new Sale(null, null, "address", null, null);
+        assertEquals("address", sale.getAddress());
+    }
+
+    @Test
+    void setAddress() {
+        Sale sale = new Sale(null, null, null, null, null);
+        sale.setAddress("address");
+        assertEquals("address", sale.getAddress());
+    }
+
+    @Test
+    void getState() {
+        Sale sale = new Sale(null, null, null, "To confirm", null);
+        assertEquals("To confirm", sale.getState());
+    }
+
+    @Test
+    void setState() {
+        Sale sale = new Sale(null, null, null, null, null);
+        sale.setState("Confirmed");
+        assertEquals("Confirmed", sale.getState());
+    }
+    @Test
+    void getPlatform() {
+        Sale sale = new Sale(null, null, null, null, "platform");
+        assertEquals("platform", sale.getPlatform());
+    }
+    @Test
+    void setPlatform() {
+        Sale sale = new Sale(null, null, null, null, null);
+        sale.setPlatform("platform");
+        assertEquals("platform", sale.getPlatform());
+    }
+
+    @Test
+    void getEmail() {
+        Sale sale = new Sale(null, "email", null, null, null);
+        assertEquals("email", sale.getEmail());
+    }
+    @Test
+    void setEmail() {
+        Sale sale = new Sale(null, null, null, null, null);
+        sale.setEmail("email");
+        assertEquals("email", sale.getEmail());
+    }
 }
