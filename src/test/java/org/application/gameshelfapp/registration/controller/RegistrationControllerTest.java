8326f0031576fb64f7e68b7c0244cfc17377e15f
AlessandroZunica <alzu2500@gmail.com>
2024-07-06 17:32:07 +0200

    removed error

--- src/test/java/org/application/gameshelfapp/registration/controller/RegistrationControllerTest.java
+++ src/test/java/org/application/gameshelfapp/registration/controller/RegistrationControllerTest.java
@@ -7,7 +7,7 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-public class RegistrationControllerTest {
+class RegistrationControllerTest {
 
     @Test
     void registrationTest(){
